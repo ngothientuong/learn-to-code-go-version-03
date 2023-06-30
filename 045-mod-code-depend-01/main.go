dd130eb17f90c18e0ab8f6b733cb1280587991cc
Tuong Ngo <ngothientuong@gmail.com>
2023-06-30 16:28:36 -0400

    drop by drop, the bucket gets filled

---
+++ 045-mod-code-depend-01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+
+	//also like this
+	fmt.Println(puppy.Bark(), "\n", puppy.Barks(), "\n", s3, "\n", s4)
+}
