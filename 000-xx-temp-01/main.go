99e5ccb69b38709030ce1552a379334572715c23
Tuong Ngo <ngothientuong@gmail.com>
2023-07-02 16:56:23 -0400

    reforked

---
+++ 000-xx-temp-01/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	xi := []int{1,2,3}
+	fmt.Println(xi[0])
+	fmt.Println(&xi[0])
+}
--- 067-select-statements/main.go
+++ 067-select-statements/main.go
@@ -25,21 +25,15 @@ func main() {
 	// https://pkg.go.dev/time#pkg-constants
 	d1 := time.Duration(rand.Int63n(250))
 	d2 := time.Duration(rand.Int63n(250))
+	// fmt.Printf("%v \t %T\n", d1, d1)
+	// time.Sleep(d1 * time.Millisecond)
+	// fmt.Println("Done")
 
-	fmt.Printf("d1 is %v \t %T\n", d1, d1)
-	time.Sleep(d1 * time.Millisecond)
-	fmt.Println("Done")
-
-	fmt.Printf("d2 is %v \t %T\n", d2, d2)
-	time.Sleep(d2 * time.Millisecond)
-	fmt.Println("Done")
-
-	// Create a 1st goroutine on a channel, make the 1st goroutine sleep for amount of time
 	go func() {
 		time.Sleep(d1 * time.Millisecond)
 		ch1 <- 41
 	}()
-	// Create a 2nd goroutine on a channel that runs concurrently relatively to above 1st goroutine, make the 2nd goroutine sleep for amount of time
+
 	go func() {
 		time.Sleep(d2 * time.Millisecond)
 		ch2 <- 42
@@ -48,7 +42,6 @@ func main() {
 	// A "select" statement chooses which of a set of possible send or receive operations will proceed.
 	// It looks similar to a "switch" statement but with the cases all referring to communication operations.
 	// https://go.dev/ref/spec#Select_statements
-	// Whichever channel comes back witha value first, select will pick that case!!!
 	select {
 	case v1 := <-ch1:
 		fmt.Println("value from channel 1", v1)
